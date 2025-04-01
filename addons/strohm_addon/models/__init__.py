a44ef9aa822f2c208b10f1846d02eb2f0171c99f
adamsocrat  <ycs@min2s.com>
2025-04-01 21:44:10 +0200

    Removed unnecessary store of backend URL's

--- addons/strohm_addon/models/__init__.py
+++ addons/strohm_addon/models/__init__.py
@@ -1,2 +1,1 @@
 from . import res_users_apikeys
-from . import res_config_settings
--- addons/strohm_addon/models/res_config_settings.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    backend_external_url = fields.Char(string="Backend External URL", config_parameter='strohm.backend_external', readonly=True)
-    backend_internal_url = fields.Char(string="Backend Internal URL", config_parameter='strohm.backend_internal', readonly=True)
